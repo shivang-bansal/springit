27664e637cdef7409803f865ae6e5bd34220a6c5
shivang-bansal <shivangbansal400@gmail.com>
2021-06-03 12:35:16 +0530

    Adding Mysql database and adding some entries in it

--- src/main/java/com/shiv/springit/SpringitApplication.java
+++ src/main/java/com/shiv/springit/SpringitApplication.java
@@ -1,14 +1,38 @@
 package com.shiv.springit;
 
+import com.shiv.springit.model.Comment;
+import com.shiv.springit.model.Link;
+import com.shiv.springit.repository.CommentRepository;
+import com.shiv.springit.repository.LinkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringitApplication {
+
+	private static final Logger log= LoggerFactory.getLogger(SpringitApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
+		return args -> {
+			Link link=new Link("Hello from SpringBoot","https://spring.io/");
+			linkRepository.save(link);
+
+			Comment comment=new Comment("This link is awesome",link);
+			commentRepository.save(comment);
+			link.addComment(comment);
+
+
+		};
+	}
 }
--- src/main/java/com/shiv/springit/model/Comment.java
+++ src/main/java/com/shiv/springit/model/Comment.java
@@ -2,6 +2,8 @@ package com.shiv.springit.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,15 +13,19 @@ import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Data
 public class Comment extends Auditable{
 
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String body;
 
+    @NonNull
     @ManyToOne
     private Link link;
+
 
 }
